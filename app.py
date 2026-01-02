10417916e8a2a4109d8dac9d7daf6ff48e6b20f2
Michael Ngo <mingo@loc.gov>
2026-01-01 17:37:19 -0800

    flask listen

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ app.register_blueprint(tournament_players_bp)
 
 if __name__ == "__main__":
     debug_mode = os.getenv("ENVIRONMENT", "prod") == "dev"
-    app.run(debug=debug_mode, port=8080)
+    app.run(debug=debug_mode, port=8080, host="0.0.0.0")
